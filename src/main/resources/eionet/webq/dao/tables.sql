e102e3e5c7d51fe9fd9f12b31222e714d98a41d6
Anton Dmitrijev <anton@tripledev.ee>
2013-08-15 14:52:10 +0000

    add project file table. Refs #15650.

--- src/main/resources/eionet/webq/dao/tables.sql
+++ src/main/resources/eionet/webq/dao/tables.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS USER_XML(
 id int AUTO_INCREMENT,
-user_id varchar(255) CHARACTER SET utf8,
-file_name varchar(255) CHARACTER SET utf8,
-xml_schema varchar(255) CHARACTER SET utf8,
+user_id varchar(2000) CHARACTER SET utf8,
+file_name varchar(2000) CHARACTER SET utf8,
+xml_schema varchar(2000) CHARACTER SET utf8,
 xml blob,
 file_size_in_bytes bigint,
 created datetime default current_timestamp,
@@ -14,4 +14,17 @@ CREATE TABLE IF NOT EXISTS PROJECT_FOLDER(
   project_id varchar(255) CHARACTER SET utf8 unique,
   description varchar(2000) CHARACTER SET utf8,
   created datetime default current_timestamp,
+  primary key (id));
+
+CREATE TABLE IF NOT EXISTS PROJECT_FILE(
+  id int AUTO_INCREMENT,
+  title varchar(255) CHARACTER SET utf8,
+  file blob,
+  xml_schema varchar(2000) CHARACTER SET utf8,
+  description varchar(2000) CHARACTER SET utf8,
+  user_name varchar(2000) CHARACTER SET utf8,
+  status boolean,
+  main_webform boolean,
+  created datetime default current_timestamp,
+  updated datetime default current_timestamp,
   primary key (id));
--- src/test/resources/eionet/webq/dao/test-tables.sql
+++ src/test/resources/eionet/webq/dao/test-tables.sql
@@ -13,3 +13,15 @@ CREATE TABLE IF NOT EXISTS PROJECT_FOLDER(
   project_id varchar2(255) unique,
   description varchar2(2000),
   created datetime default current_timestamp);
+
+CREATE TABLE IF NOT EXISTS PROJECT_FILE(
+  id identity primary key,
+  title varchar2(255),
+  file blob,
+  xml_schema varchar2(2000),
+  description varchar2(2000),
+  user_name varchar2(2000),
+  status boolean,
+  main_webform boolean,
+  created datetime default current_timestamp,
+  updated datetime default current_timestamp);
