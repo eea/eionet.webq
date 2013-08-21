f81c09e333368a74986abf2f07d9b9cfa1d5e946
Anton Dmitrijev <anton@tripledev.ee>
2013-08-21 11:49:25 +0000

    set max_allowed_packet to 10mb, use medium blob for mysql(up to 16 mb). Refs #15634.

--- src/main/resources/eionet/webq/dao/tables.sql
+++ src/main/resources/eionet/webq/dao/tables.sql
@@ -1,9 +1,11 @@
+-- SET GLOBAL max_allowed_packet = 10485760; -- 10 MB
+
 CREATE TABLE IF NOT EXISTS USER_XML(
 id int AUTO_INCREMENT,
 user_id varchar(2000) CHARACTER SET utf8,
 file_name varchar(2000) CHARACTER SET utf8,
 xml_schema varchar(2000) CHARACTER SET utf8,
-xml blob,
+xml mediumblob,
 file_size_in_bytes bigint,
 created datetime default current_timestamp,
 updated datetime default current_timestamp,
@@ -20,7 +22,7 @@ CREATE TABLE IF NOT EXISTS PROJECT_FILE(
   id int AUTO_INCREMENT,
   project_id int,
   title varchar(255) CHARACTER SET utf8,
-  file blob,
+  file mediumblob,
   xml_schema varchar(2000) CHARACTER SET utf8,
   description varchar(2000) CHARACTER SET utf8,
   user_name varchar(2000) CHARACTER SET utf8,
