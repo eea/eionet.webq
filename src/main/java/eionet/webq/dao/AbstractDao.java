8324d19b64bc75265097e28ff49a31bbeee5ba3e
Enriko Käsper <enriko.kasper@gmail.com>
2013-08-23 14:40:52 +0000

    Checkstyle fix.

--- src/main/java/eionet/webq/dao/AbstractDao.java
+++ src/main/java/eionet/webq/dao/AbstractDao.java
@@ -1,10 +1,10 @@
 package eionet.webq.dao;
 
+import java.util.Properties;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
-
-import java.util.Properties;
 
 /**
  * Common logic for DAO classes.
@@ -20,11 +20,11 @@ public abstract class AbstractDao<T> {
     @Qualifier("sql_statements")
     Properties sqlProperties;
 
-
     /**
      * Row mapper for web form upload.
      *
-     * @return {@link org.springframework.jdbc.core.BeanPropertyRowMapper} for {@link org.springframework.jdbc.core.BeanPropertyRowMapper}
+     * @return {@link org.springframework.jdbc.core.BeanPropertyRowMapper} for
+     *         {@link org.springframework.jdbc.core.BeanPropertyRowMapper}
      */
     BeanPropertyRowMapper<T> rowMapper() {
         return BeanPropertyRowMapper.newInstance(getDtoClass());
