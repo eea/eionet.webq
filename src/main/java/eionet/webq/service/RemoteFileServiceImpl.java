0a7aba14d931d02297fb3911fa9ae182f45d3fa9
Enriko Käsper <enriko@tripledev.ee>
2013-10-01 10:50:31 +0300

    Make the RemoteFileService to support "file://" scheme beside "http://".

--- src/main/java/eionet/webq/service/RemoteFileServiceImpl.java
+++ src/main/java/eionet/webq/service/RemoteFileServiceImpl.java
@@ -20,13 +20,18 @@
  */
 package eionet.webq.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.zip.CRC32;
+
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestOperations;
-
-import java.util.zip.CRC32;
 
 /**
  * {@link eionet.webq.service.RemoteFileService} implementation.
@@ -41,11 +46,16 @@ public class RemoteFileServiceImpl implements RemoteFileService {
 
     @Override
     public byte[] fileContent(String remoteFileUrl) throws FileNotAvailableException {
-        ResponseEntity<byte[]> download = downloader.getForEntity(remoteFileUrl, byte[].class);
-        if (download.getStatusCode() != HttpStatus.OK || !download.hasBody()) {
-            throw new FileNotAvailableException("Response is not OK or body not attached for " + remoteFileUrl);
+
+        if (remoteFileUrl != null && remoteFileUrl.startsWith("file://")) {
+            return readFilesystemFile(remoteFileUrl);
+        } else {
+            ResponseEntity<byte[]> download = downloader.getForEntity(remoteFileUrl, byte[].class);
+            if (download.getStatusCode() != HttpStatus.OK || !download.hasBody()) {
+                throw new FileNotAvailableException("Response is not OK or body not attached for " + remoteFileUrl);
+            }
+            return download.getBody();
         }
-        return download.getBody();
     }
 
     @Override
@@ -56,6 +66,7 @@ public class RemoteFileServiceImpl implements RemoteFileService {
 
     /**
      * Calculates crc32 checksum.
+     *
      * @see java.util.zip.CRC32
      *
      * @param bytes bytes to calculate checksum.
@@ -65,5 +76,24 @@ public class RemoteFileServiceImpl implements RemoteFileService {
         CRC32 crc32 = new CRC32();
         crc32.update(bytes);
         return crc32.getValue();
+    }
+
+    /**
+     * Read file content from file system. The fileUri has to start with "file://" prefix.
+     *
+     * @param fileUri file location in file system in form of URI.
+     * @return file content as bytes.
+     * @throws FileNotAvailableException if file is not available as local resource or fileUri is not valid URI.
+     */
+    private byte[] readFilesystemFile(String fileUri) throws FileNotAvailableException {
+        try {
+            return FileUtils.readFileToByteArray(new File(new URI(fileUri)));
+        } catch (IOException e) {
+            throw new FileNotAvailableException("The file is not available at: " + fileUri + "." + e.toString());
+        } catch (URISyntaxException e) {
+            throw new FileNotAvailableException("Incorrect file URI: " + fileUri + "." + e.toString());
+        } catch (Exception e) {
+            throw new FileNotAvailableException("Illegal file URI argument: " + fileUri + "." + e.toString());
+        }
     }
 }
