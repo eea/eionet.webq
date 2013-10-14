44ba21db78eb448c3c6640c89895d367489007b2
Anton Dmitrijev <anton@tripledev.ee>
2013-10-14 12:24:16 +0300

    merge modules storage functionality draft. Refs #17083

---
+++ src/main/java/eionet/webq/dao/MergeModulesImpl.java
@@ -0,0 +1,53 @@
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is Web Questionnaires 2
+ *
+ * The Initial Owner of the Original Code is European Environment
+ * Agency. Portions created by TripleDev are Copyright
+ * (C) European Environment Agency.  All Rights Reserved.
+ *
+ * Contributor(s):
+ *        Anton Dmitrijev
+ */
+package eionet.webq.dao;
+
+import eionet.webq.dao.orm.MergeModule;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+
+/**
+ * {@link eionet.webq.dao.MergeModules} implementation.
+ */
+@Repository
+public class MergeModulesImpl extends AbstractDao<MergeModule> implements MergeModules {
+
+    @Override
+    public Collection<MergeModule> findAll() {
+        return null;
+    }
+
+    @Override
+    public int save(MergeModule module) {
+        return (Integer) getCurrentSession().save(module);
+    }
+
+    @Override
+    public MergeModule findById(int id) {
+        return (MergeModule) getCurrentSession().byId(getDtoClass()).load(id);
+    }
+
+    @Override
+    Class<MergeModule> getDtoClass() {
+        return MergeModule.class;
+    }
+}
---
+++ src/test/java/eionet/webq/dao/MergeModulesTest.java
@@ -0,0 +1,49 @@
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is Web Questionnaires 2
+ *
+ * The Initial Owner of the Original Code is European Environment
+ * Agency. Portions created by TripleDev are Copyright
+ * (C) European Environment Agency.  All Rights Reserved.
+ *
+ * Contributor(s):
+ *        Anton Dmitrijev
+ */
+package eionet.webq.dao;
+
+import configuration.ApplicationTestContextWithMockSession;
+import eionet.webq.dao.orm.MergeModule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ApplicationTestContextWithMockSession.class})
+@Transactional
+public class MergeModulesTest {
+
+    @Autowired
+    private MergeModules mergeModules;
+
+    @Test
+    public void savesMergeModuleToStorage() throws Exception {
+        MergeModule mergeModule = new MergeModule();
+        int id = mergeModules.save(mergeModule);
+
+        assertNotNull(mergeModules.findById(id));
+    }
+}
