2c1793ac8a758e405eb76d804f5c9067f43a463b
Anton Dmitrijev <anton@tripledev.ee>
2013-12-02 14:42:49 +0200

    add remote web forms service. Refs #17613

---
+++ src/main/java/eionet/webq/service/RemoteWebFormService.java
@@ -0,0 +1,54 @@
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is Web Questionnaires 2
+ *
+ * The Initial Owner of the Original Code is European Environment
+ * Agency. Portions created by TripleDev are Copyright
+ * (C) European Environment Agency.  All Rights Reserved.
+ *
+ * Contributor(s):
+ *        Anton Dmitrijev
+ */
+package eionet.webq.service;
+
+import eionet.webq.dao.WebFormStorage;
+import eionet.webq.dao.orm.ProjectFile;
+import eionet.webq.dto.WebFormType;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Collection;
+
+/**
+ */
+//@Service("remoteWebForms")
+public class RemoteWebFormService extends AbstractWebFormsService {
+    /**
+     * Web forms storage.
+     */
+    @Autowired
+    private WebFormStorage webFormStorage;
+
+    @Override
+    public Collection<ProjectFile> getAllActiveWebForms() {
+        return webFormStorage.getAllActiveWebForms(WebFormType.REMOTE);
+    }
+
+    @Override
+    public ProjectFile findActiveWebFormById(int id) {
+        return webFormStorage.getActiveWebFormById(WebFormType.REMOTE, id);
+    }
+
+    @Override
+    protected Collection<ProjectFile> findWebFormsForNotEmptyXmlSchemas(Collection<String> xmlSchemas) {
+        return webFormStorage.findWebFormsForSchemas(WebFormType.REMOTE, xmlSchemas);
+    }
+}
